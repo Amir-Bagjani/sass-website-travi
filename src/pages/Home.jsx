5d7ba7df1c60533a0478dfa0756457f89b1e3e86
amirbagjni <amirbagjani@gmail.com>
2022-03-10 20:31:14 +0330

    landing - 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import "../styles/home.scss"
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">
+            Developer Connector
+          </h1>
+          <p className="lead">
+            Create developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className="buttons">
+            <a href="register" className="btn btn-primary">Sign Up</a>
+            <a href="login" className="btn btn-primary">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
