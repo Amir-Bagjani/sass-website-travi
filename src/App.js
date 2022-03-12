6210d6c9e2a88ddea26cc0c396950f4d136d6280
amirbagjni <amirbagjani@gmail.com>
2022-03-13 03:24:22 +0330

    profiles add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Profiles from "./pages/Profiles";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profiles />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <section className="container">
-    <h1 className="large text-primary">Sign In</h1>
-    <p className="lead">
-      <i className="fas fa-user"></i>Sign into your Account
-    </p>
+      <div className="alert alert-danger">Invalid credentials</div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Sign into your Account
+      </p>
 
-    <form className="form">
-      <div className="form-group">
-        <input type="email" placeholder="Email" required/>
-      </div>
-      <div className="form-group">
-        <input type="password" placeholder="Password" minLength={6} />
-      </div>
-      <input type="submit" value="Sign In" className="btn btn-primary" />
-    </form>
-    <p className="my-1">Don't have an account? <Link to="/register">Sign Up</Link></p>
-  </section>
-  )
-}
+      <form className="form">
+        <div className="form-group">
+          <input type="email" placeholder="Email" required />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" minLength={6} />
+        </div>
+        <input type="submit" value="Sign In" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
