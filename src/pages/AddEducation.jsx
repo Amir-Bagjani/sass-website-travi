5465716def82725703cc3788fa5af74696e4cb96
amirbagjni <amirbagjani@gmail.com>
2022-03-14 19:23:10 +0330

    add education finish

--- src/pages/AddEducation.jsx
+++ src/pages/AddEducation.jsx
@@ -3,9 +3,60 @@ import React from 'react'
 const AddEducation = () => {
   return (
     <section className="container">
-        education
+         <h1 className="large text-primary">
+        Add Your Education
+      </h1>
+      <p className="lead">
+        <i className="fas fa-graduation-cap"></i> Add any school, bootcamp, etc that
+        you have attended
+      </p>
+      <small>* = required field</small>
+      <form className="form">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* School or Bootcamp"
+            name="school"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree or Certificate"
+            name="degree"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input type="text" placeholder="Field Of Study" name="fieldofstudy" />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input type="date" name="from" />
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input type="date" name="to" />
+        </div>
+        <div className="form-group">
+          <p>
+            <input type="checkbox" name="current" value="" /> Current School
+          </p>
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Program Description"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary my-1">Submit</button>
+        <a className="btn my-1" href="dashboard.html">Go Back</a>
+      </form>
     </section>
   )
 }
 
-export default AddEducation
+export default AddEducation;
